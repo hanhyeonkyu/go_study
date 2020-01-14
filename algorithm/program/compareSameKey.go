31e63fb5c9de6460c2c791932c7ea7e190e27894
hanhyeonkyu <busgod1212@gmail.com>
2020-01-14 14:26:46 +0900

    next error so stop

---
+++ algorithm/program/compareSameKey.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var num int
+	fmt.Scanln(&num)
+	var scanner = bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var heights string = scanner.Text()
+	var heightArr = strings.Split(heights, " ")
+	heightArr = heightArr[:num]
+	scanner.Scan()
+	var months string = scanner.Text()
+	var monthArr = strings.Split(months, " ")
+	monthArr = monthArr[:num]
+	var targetMonth string
+	fmt.Scanln(&targetMonth)
+	var index int
+	for p := num - 1; p >= 0; p-- {
+		if monthArr[p] == targetMonth {
+			index = p
+			break
+		}
+	}
+	fmt.Println(heightArr[index])
+}
